e03c37bbbd1860cc8c818371cf21aafb98f87852
Francisco Castillo <frankycstl@gmail.com>
2023-02-21 21:15:02 -0800

    initial files commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Info from './components/Info'
+import  About from './components/About'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
+
+import emailIcon from "./assets/email.gif"
+import linkedInIcon from "./assets/linkedinn.gif"
+// import img from "../public/portrait.JPG"
+
+function App () {
+  return (
+    <div className='container'>
+      <Info 
+        emailIcon={emailIcon}
+        linkedInIcon={linkedInIcon}
+        image="../public/portrait.JPG"
+      />
+      <About />
+      <Interests />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
